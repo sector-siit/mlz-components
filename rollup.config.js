65fe3008cb9a2471667fde24c7a70445386e7ff7
Pablo Ivan Lugo Aquino <93728863+plugo1@users.noreply.github.com>
2025-11-07 18:22:42 -0300

    chore: update dependencies and configure PostCSS in Rollup
    - Added autoprefixer and rollup-plugin-postcss to package.json
    - Updated postcss.config.cjs to remove unnecessary plugin
    - Integrated rollup-plugin-postcss into rollup.config.js with configuration for CSS processing

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = JSON.parse(readFileSync("./package.json", "utf8"));
 
@@ -25,6 +26,16 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({
+        config: {
+          path: "./postcss.config.js",
+        },
+        extensions: [".css"],
+        minimize: true,
+        inject: {
+          insertAt: "top",
+        },
+      }),
     ],
     external: ["react", "react-dom"],
   },
